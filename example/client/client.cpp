120ee40051d85228361c3cc945de7346c3376fce
xueshen.an <xueshen.an@goertek.com>
2024-03-01 09:28:14 +0800

    update example to support new ftp implement

--- example/client/client.cpp
+++ example/client/client.cpp
@@ -10,10 +10,11 @@
 #include <thread>
 #include <vector>
 
-static std::string download_camera_definition_file_by_ftp(std::shared_ptr<mavsdk::System> system);
+static std::string download_camera_definition_file_by_ftp(std::shared_ptr<mavsdk::System> system,
+                                                          std::string file_uri);
 
 static void do_camera_operation(mavsdk::Camera &camera);
-static void do_camear_settings(mavsdk::Camera &camera);
+static void do_camera_settings(mavsdk::Camera &camera);
 
 static inline void set_camera_settings(mavsdk::Camera &camera, const std::string &name,
                                        const std::string &value);
@@ -22,10 +23,8 @@ static inline std::string get_camera_setting(mavsdk::Camera &camera, const std::
 int main(int argc, const char *argv[]) {
     // we run client plugins to act as the GCS
     // to communicate with the autopilot server plugins.
-    mavsdk::Mavsdk mavsdk;
-    mavsdk::Mavsdk::Configuration configuration(
-        mavsdk::Mavsdk::Configuration::UsageType::GroundStation);
-    mavsdk.set_configuration(configuration);
+    mavsdk::Mavsdk mavsdk{
+        mavsdk::Mavsdk::Configuration{mavsdk::Mavsdk::ComponentType::GroundStation}};
 
     auto result = mavsdk.add_any_connection("udp://:14550");
     if (result == mavsdk::ConnectionResult::Success) {
@@ -38,8 +37,8 @@ int main(int argc, const char *argv[]) {
         mavsdk.subscribe_on_new_system([&mavsdk, &prom, &handle]() {
             auto system = mavsdk.systems().back();
 
-            if (system->has_autopilot()) {
-                std::cout << "Discovered autopilot from Client" << std::endl;
+            if (system->has_camera()) {
+                std::cout << "Discovered camera from Client" << std::endl;
 
                 // Unsubscribe again as we only want to find one system.
                 mavsdk.unsubscribe_on_new_system(handle);
@@ -50,17 +49,25 @@ int main(int argc, const char *argv[]) {
         });
 
     if (fut.wait_for(std::chrono::seconds(10)) == std::future_status::timeout) {
-        std::cout << "No autopilot found, exiting" << std::endl;
+        std::cout << "No camera found, exiting" << std::endl;
         return -1;
     }
 
     auto system = fut.get();
 
+    auto prom_definition = std::promise<std::string>{};
+    auto fut_definition = prom_definition.get_future();
     auto camera = mavsdk::Camera{system};
-    camera.subscribe_information([](mavsdk::Camera::Information info) {
-        std::cout << "Camera information:" << std::endl;
-        std::cout << info << std::endl;
-    });
+    std::atomic<bool> already_get_definition{false};
+    camera.subscribe_information(
+        [&prom_definition, &already_get_definition](mavsdk::Camera::Information info) {
+            std::cout << "Camera information:" << std::endl;
+            std::cout << info << std::endl;
+            if (!info.definition_file_uri.empty() && !already_get_definition) {
+                already_get_definition = true;
+                prom_definition.set_value(info.definition_file_uri);
+            }
+        });
 
     camera.subscribe_video_stream_info(
         [](std::vector<mavsdk::Camera::VideoStreamInfo> video_stream_infos) {
@@ -75,13 +82,22 @@ int main(int argc, const char *argv[]) {
         std::cout << status << std::endl;
     });
 
-    do_camera_operation(camera);
-
-    std::string define_data = download_camera_definition_file_by_ftp(system);
-    if (define_data.size() > 0) {
-        camera.set_definition_data(define_data);
-        do_camear_settings(camera);
+    if (fut_definition.wait_for(std::chrono::seconds(1)) == std::future_status::timeout) {
+        std::cout << "Not definition file found" << std::endl;
+    } else {
+        std::string file_uri = fut_definition.get();
+        if (file_uri.find("ftp://") == 0) {
+            file_uri = file_uri.substr(6);
+            std::string define_data = download_camera_definition_file_by_ftp(system, file_uri);
+            if (define_data.size() > 0) {
+                auto result = camera.set_definition_data(define_data);
+                std::cout << "set camera definition data result : " << result << std::endl;
+                do_camera_settings(camera);
+            }
+        }
     }
+
+    // do_camera_operation(camera);
     while (true) {
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
@@ -127,32 +143,34 @@ static void do_camera_operation(mavsdk::Camera &camera) {
 }
 
 // demo for use mavlink ftp to download camera config file
-static std::string download_camera_definition_file_by_ftp(std::shared_ptr<mavsdk::System> system) {
+static std::string download_camera_definition_file_by_ftp(std::shared_ptr<mavsdk::System> system,
+                                                          std::string file_uri) {
     auto ftp = mavsdk::Ftp{system};
-    const std::string camera_define_file_name = "C10.xml";
-    std::filesystem::path full_path = std::filesystem::current_path().append("build");
-
-    std::cout << "Download camera define file to " << full_path << std::endl;
+    std::filesystem::path download_path = std::filesystem::current_path().append("build");
+    // std::string path
+    std::cout << "Download camera define file to " << download_path << std::endl;
     auto prom = std::promise<std::string>{};
     auto fut = prom.get_future();
-    ftp.download_async(camera_define_file_name, full_path,
-                       [&full_path, &camera_define_file_name, &prom](
-                           mavsdk::Ftp::Result result, mavsdk::Ftp::ProgressData progress_data) {
-                           if (result == mavsdk::Ftp::Result::Success) {
-                               std::cout << "download camera config file success";
-                               std::string file_path_with_name = full_path;
-                               file_path_with_name += "/" + camera_define_file_name;
-                               std::cout << file_path_with_name << std::endl;
-                               std::ifstream file_stream(file_path_with_name);
-                               std::stringstream buffer;
-                               buffer << file_stream.rdbuf();
-                               prom.set_value(buffer.str());
-                           } else if (result != mavsdk::Ftp::Result::Next) {
-                               std::cout << "Download definition file failed : " << result
-                                         << std::endl;
-                               prom.set_value("");
-                           }
-                       });
+    ftp.download_async(
+        file_uri, download_path, false,
+        [&download_path, &file_uri, &prom](mavsdk::Ftp::Result result,
+                                           mavsdk::Ftp::ProgressData progress_data) {
+            if (result == mavsdk::Ftp::Result::Success) {
+                std::cout << "download camera config file success" << std::endl;
+                std::string file_path_with_name = download_path;
+                file_path_with_name += "/" + file_uri.substr(file_uri.rfind("/"));
+                std::cout << "download file in path " << file_path_with_name << std::endl;
+                std::ifstream file_stream(file_path_with_name);
+                std::stringstream buffer;
+                buffer << file_stream.rdbuf();
+                prom.set_value(buffer.str());
+            } else if (result == mavsdk::Ftp::Result::Next) {
+                std::cout << "Download progress: " << progress_data.bytes_transferred << "/"
+                          << progress_data.total_bytes << " bytes" << std::endl;
+            } else {
+                std::cout << "Download result : " << result << std::endl;
+            }
+        });
 
     if (fut.wait_for(std::chrono::seconds(10)) == std::future_status::timeout) {
         std::cout << "Download camera define file failed" << std::endl;
@@ -162,14 +180,14 @@ static std::string download_camera_definition_file_by_ftp(std::shared_ptr<mavsdk
     return fut.get();
 }
 
-static void do_camear_settings(mavsdk::Camera &camera) {
+static void do_camera_settings(mavsdk::Camera &camera) {
     std::vector<std::pair<std::string, std::string>> settings;
-    settings.push_back({"CAM_WBMODE", "1"});
-    settings.push_back({"CAM_EXPMODE", "0"});
+    // settings.push_back({"CAM_WBMODE", "1"});
+    // settings.push_back({"CAM_EXPMODE", "0"});
     settings.push_back({"CAM_EV", "2.0"});
-    settings.push_back({"CAM_EXPMODE", "1"});
-    settings.push_back({"CAM_SHUTTERSPD", "0.016666"});
-    settings.push_back({"CAM_ISO", "6400"});
+    // settings.push_back({"CAM_EXPMODE", "1"});
+    // settings.push_back({"CAM_SHUTTERSPD", "0.016666"});
+    // settings.push_back({"CAM_ISO", "6400"});
 
     for (auto &it : settings) {
         set_camera_settings(camera, it.first, it.second);
@@ -202,5 +220,7 @@ static inline std::string get_camera_setting(mavsdk::Camera &camera, const std::
     mavsdk::Camera::Setting setting;
     setting.setting_id = name;
     auto [result, out_setting] = camera.get_setting(setting);
+    std::cout << "get " << out_setting.setting_id << " value : " << out_setting.option.option_id
+              << " result : " << result << std::endl;
     return out_setting.option.option_id;
 }
